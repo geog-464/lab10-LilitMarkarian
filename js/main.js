8fcc35f38fcecf35307c74f1efe7fa4cd4244799
Lilit Markarian <lilmar444@gmail.com>
2022-04-13 23:30:41 -0400

    Lab10-submitted

--- js/main.js
+++ js/main.js
@@ -2,38 +2,121 @@
 let myMap;
 
 // we might as well declare our baselayer(s) here too
-const CartoDB_Positron = L.tileLayer(
-	'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
-	{
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-	}
-)
+const OpenStreetMap_Mapnik = L.tileLayer(
+	'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
 function initialize(){
     loadMap();
 };
 
-function loadMap(){
-	
-	//now reassign the map variable by actually making it a useful object, this will load your leaflet map
-	myMap = L.map('mapdiv', {
-		center: [45.50, -73.58]
-		,zoom: 3
-		,maxZoom: 18
-		,minZoom: 3
-		,layers: CartoDB_Positron
-	});
+function fetchData(){
+    //load the data
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json,
+								{style: styleAll, pointToLayer: generateCircles, onEachFeature: addPopups}).addTo(myMap);
+        })
+};
+
+function loadMap(mapid){
+	try {
+		myMap.remove()
+	}
+	catch(e) {
+		console.log(e)
+		console.log("no map to delete")
+	}
+	finally {
+
+		if (mapid=='Stations'){
+		myMap = L.map('mapdiv', {
+			center: [45.50, -73.58]
+			,zoom: 3
+			,maxZoom: 15
+			,minZoom: 3
+			,layers: OpenStreetMap_Mapnik
+			});
+
+			fetchData(url='https://raw.githubusercontent.com/geog-464/lab10/main/data/Amtrak_Stations.geojson');
+		}
+
+		if (mapid=="Megacities") {
+			myMap = L.map('mapdiv', {
+				center: [31.24967, 30.06263]
+				,zoom: 2
+				,maxZoom: 15
+				,minZoom: 1
+				,layers: OpenStreetMap_Mapnik
+			});
+			fetchData(url='https://raw.githubusercontent.com/geog-464/lab10/main/data/megacities.geojson');
+		}
+	}
 
 	//add the basemap style(s) to a JS object, to which you could also add other baselayers. This object is loaded as a basemap selector as seen further down
 	let baseLayers = {
-		"CartoDB": CartoDB_Positron
-		//,...
+		"OpenStreetMap.Mapnik": OpenStreetMap_Mapnik
 	};
 
 	//declare basemap selector widget
 	let lcontrol = L.control.layers(baseLayers);
 	//add it to the map
 	lcontrol.addTo(myMap);
+
+	fetchData();
+
+	console.log(mapid)
+
 };
+
+function generateCircles(feature, latlng) {
+	return L.circleMarker(latlng);
+}
+
+
+function styleAll(feature, latlng) {
+
+	var styles = {
+		dashArray:null,
+		dashOffset:null,
+  	lineJoin:null,
+		lineCap:null,
+	  stroke:false,
+	  color:'#000',
+		opacity:1,
+		weight:1,
+		fillColor:null,
+		fillOpacity:0
+	};
+
+	if (feature.geometry.type == "Point") {
+		styles.fillColor = '#F1A99B'
+		,styles.fillOpacity = 0.3
+		,styles.stroke=true
+		,styles.radius=10
+	}
+
+	if (typeof feature.properties.ZipCode == 'string') {
+		styles.fillColor = '#65b5ab'
+		,styles.fillOpacity = 0.5
+		,styles.stroke=true
+		,styles.radius=10
+	}
+
+	return styles;
+}
+
+function addPopups(feature, layer){
+
+	layer.bindPopup();
+}
+
+
 
 window.onload = initialize();
